a44a1da2dd5d50f7b673805ce4f81c59daa4df73
Devraj <debarajkatuwal@gmail.com>
2022-09-03 20:45:26 -0400

    finish filters.

--- blango/settings.py
+++ blango/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'blango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/ 'blango/templates'],
+        'DIRS': [BASE_DIR/ 'blog/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
